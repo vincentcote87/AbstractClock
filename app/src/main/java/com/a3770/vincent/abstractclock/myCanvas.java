dfa814c42ff16fb73fbfdefb3ef1dd1f12898fb2
Vincent <vincentcote87@gmail.com>
2018-03-03 01:14:34 -0700

    clock main functionality works, changes colours based on time

--- app/src/main/java/com/a3770/vincent/abstractclock/myCanvas.java
+++ app/src/main/java/com/a3770/vincent/abstractclock/myCanvas.java
@@ -2,15 +2,14 @@ package com.a3770.vincent.abstractclock;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
+//import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+//import android.graphics.Rect;
 import android.icu.util.Calendar;
 import android.util.AttributeSet;
 import android.view.View;
 
-import static android.graphics.Color.BLUE;
-import static android.graphics.Color.RED;
+import static android.graphics.Color.rgb;
 
 /**
  * Created by Vincent on 3/2/2018.
@@ -24,6 +23,14 @@ public class myCanvas extends View {
     int screenHeight;
     int barWidth;
     int barHeight;
+
+    int hour;
+    int min;
+    int sec;
+
+    int red;
+    int green;
+    int blue;
 
 
     public myCanvas(Context context) {
@@ -40,15 +47,10 @@ public class myCanvas extends View {
 
     private void init() {
         paint = new Paint();
-//        paint.setColor(BLUE);
-//        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-
         screenWidth = getWidth();
         screenHeight = getHeight();
-//        barWidth = (screenWidth - 120) / 3;
         barWidth = (screenWidth) / 3;
         barHeight = (int)Math.round(screenHeight * 0.75);
-
         init = true;
     }
 
@@ -56,6 +58,11 @@ public class myCanvas extends View {
         if(!init) {
             init();
         }
+
+        Calendar time = Calendar.getInstance();
+        hour = time.get(Calendar.HOUR);
+        min = time.get(Calendar.MINUTE);
+        sec = time.get(Calendar.SECOND);
 
         canvas.drawColor(0xff000000);
         drawHour(canvas);
@@ -66,95 +73,72 @@ public class myCanvas extends View {
         invalidate();
     }
 
+    private void getColour(int timeVar) {
+        if(timeVar == 0) {
+            red = 135;
+            green = 0;
+            blue = 255;
+        }
+        else if(timeVar > 0 && timeVar < 15) {
+            red = 135 - ((timeVar % 15) * 9);
+            green = 0;
+            blue = 255;
+        }
+        else if(timeVar >= 15 && timeVar < 30) {
+            red = 0;
+            green = ((timeVar % 15) * 13);
+            blue = 255;
+        }
+        else if(timeVar >=30 && timeVar < 45) {
+            red = 0;
+            green = 195 + ((timeVar % 15) * 4);
+            blue = 255 - ((timeVar % 15) * 15);
+        }
+        else if(timeVar >=45 && timeVar < 60) {
+            red = (timeVar % 15) * 17;
+            green = 255;
+            blue = 30 - ((timeVar % 15) * 2);
+        }
+        else {
+            red = 255;
+            green = 255;
+            blue = 0;
+        }
+    }
+
     private void drawSec(Canvas canvas) {
         paint.reset();
-        paint.setColor(0xfff34faf);
+        Paint pt = new Paint();
+        pt.setColor(rgb(255,255,255));
+        pt.setStyle(Paint.Style.FILL_AND_STROKE);
+        pt.setTextSize(100);
+        canvas.drawText(Integer.toString(sec), 300, 200, pt);
+        getColour(sec);
+        paint.setColor(rgb(red, green, blue));
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
-//        canvas.drawRect(90 + 2*barWidth, 100, 90 + 3*barWidth, barHeight, paint);
         canvas.drawRect(2*barWidth, 100,3*barWidth, barHeight, paint);
     }
 
     private void drawMin(Canvas canvas) {
         paint.reset();
-        paint.setColor(0xfff6bf3f);
+        getColour(min);
+        paint.setColor(rgb(red, green, blue));
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
-//        canvas.drawRect(60 + barWidth, 100, 60 + 2*barWidth, barHeight, paint);
         canvas.drawRect(barWidth, 100, 2*barWidth, barHeight, paint);
     }
 
     private void drawHour(Canvas canvas) {
         paint.reset();
-        paint.setColor(0xffffffff);
+        getColour(hour * 5);
+        paint.setColor(rgb(red, green, blue));
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
-//        canvas.drawRect(30, 100, 30 + barWidth, barHeight, paint);
         canvas.drawRect(0, 100, barWidth, barHeight, paint);
 
     }
 }
-
-//
-//public class myCanvas extends View {
-//
-//    boolean init=false;
-//    Paint pt;
-//
-//    //all three constructors are needed here
-//    public myCanvas(Context context) {
-//        super(context);
-//    }
-//
-//    public myCanvas(Context context, AttributeSet attrs)
-//    {
-//        super(context,attrs);
-//    }
-//
-//    public myCanvas(Context context, AttributeSet attrs, int defStyleAttr)
-//    {
-//        super(context, attrs, defStyleAttr);
-//    }
-//
-//    void init()
-//    {
-//        pt = new Paint();
-//        pt.setColor(Color.BLACK);
-//        pt.setStyle(Paint.Style.FILL_AND_STROKE);
-//        pt.setTextSize(100);
-//        pt.setFakeBoldText(true);
-//
-//    }
-//
-//    @Override
-//    protected void onDraw(Canvas canvas){
-//
-//        if(!init){
-//            init();
-//            init=true;
-//        }
-//
-//        canvas.drawColor(Color.WHITE);
-//
-//        //get the time
-//        Calendar ctime = Calendar.getInstance();
-//
-//        String hour = Integer.toString(ctime.get(Calendar.HOUR));
-//        String min = Integer.toString(ctime.get(Calendar.MINUTE));
-//        String sec = Integer.toString(ctime.get(Calendar.SECOND));
-//
-//        if(ctime.get(Calendar.MINUTE)>=10&&ctime.get(Calendar.SECOND)>=10) {
-//            canvas.drawText(hour + ":" + min + ":" + sec, 300, 200, pt);
-//        } else if(ctime.get(Calendar.MINUTE)>=10){
-//            canvas.drawText(hour + ":" + min + ":0" + sec, 300, 200, pt);
-//        } else {
-//            canvas.drawText(hour + ":0" + min + ":" + sec, 300, 200, pt);
-//        }
-//
-//        //maybe possible to delay invalidating a bit.
-//        invalidate();
-//    }
-//}
